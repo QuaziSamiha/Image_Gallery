35dfd2baecdcc54d5ab4cd623758819d5ced2eab
Quazi Samiha <quazisamiha@gmail.com>
2023-11-04 17:19:35 -0700

    css done

--- src/components/ImageGallery/Gallery.jsx
+++ src/components/ImageGallery/Gallery.jsx
@@ -1,23 +1,16 @@
 import { FaCheckSquare, FaImage, FaSquare } from "react-icons/fa";
 import { useImageGalleryContext } from "../../contexts/ImageGalleryContext";
-import "./animation.css";
+import "./gallery.css";
 const Gallery = () => {
   const {
     hoveredImage,
     images,
     handleMouseEnter,
     handleMouseLeave,
-    handleAddImage,
     toggleImageSelection,
     minId,
   } = useImageGalleryContext();
 
-  const selectedImageBg = {
-    background: "rgba(206, 208, 212, 0.4)",
-  };
-  const hoveredImageBg = {
-    background: "rgba(130, 130, 130, 0.7)",
-  };
   return (
     <>
       {images.map((image) => (
@@ -27,9 +20,9 @@ const Gallery = () => {
           key={image.id}
           className={`${
             image.id === minId
-              ? "col-span-2 row-span-2 flex justify-center items-center p-2 relative"
-              : "flex justify-center items-center p-2 relative "
-          }`}
+              ? "col-span-2 row-span-2 "
+              : "col-span-1 row-span-1"
+          } flex justify-center items-center p-2 relative`}
         >
           <button onClick={() => toggleImageSelection(image.id)}>
             {image.selected ? (
@@ -39,26 +32,24 @@ const Gallery = () => {
             ) : null}
           </button>
           <img
-            style={
-              hoveredImage === image.id
-                ? hoveredImageBg
-                : image.selected
-                ? selectedImageBg
-                : {}
-            }
             className={`${
               image.id === minId
                 ? "h-48 md:h-60 lg:h-72 w-48 md:w-60 lg:w-72"
                 : "h-20 md:h-28 lg:h-32 w-20 md:w-28 lg:w-32"
-            } border border-[#ced0d4] rounded-lg ${
-              hoveredImage === image.id ? "flip-right-animation" : ""
-            }`}
+            }
+            ${
+              hoveredImage === image.id
+                ? "flip-right-animation hoveredImageBg"
+                : ""
+            }
+            ${image.selected ? "selectedImageBg" : ""} 
+            border border-[#ced0d4] rounded-lg`}
             src={image.image}
             alt=""
           />
         </div>
       ))}
-      <button onClick={handleAddImage}>
+      <button>
         <div className="flex justify-center items-center text-sm  p-2">
           <div className="border border-[#ced0d4] rounded-lg h-20 md:h-28 lg:h-32 w-20 md:w-28 lg:w-32">
             <FaImage className="h-6 w-6 my-2 lg:my-5 mx-5 lg:mx-12" />
